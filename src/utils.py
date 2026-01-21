a85d959125e5acf8f21c62f7929b695cce662d9f
Rana Muhammad Talha <muhammadtalhashahid2005@gmail.com>
2026-01-21 22:42:19 +0500

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,10 @@ import sys
 import numpy as np
 import pandas as pd
 
+from sklearn.metrics import r2_score
 from src.exception import CustomException
 from src.logger import logging
+
 
 def save_object(file_path, obj):
     """
@@ -24,5 +26,28 @@ def save_object(file_path, obj):
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
 
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+
+            # Train the model
+            model.fit(X_train, y_train)
+
+            # Predict on test data
+            y_pred = model.predict(X_test)
+
+            # Calculate R2 score
+            r2_square = r2_score(y_test, y_pred)
+
+            report[list(models.keys())[i]] = r2_square
+
+        return report
     except Exception as e:
         raise CustomException(e, sys)
