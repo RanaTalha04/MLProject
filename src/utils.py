13129460edb4641c64651b19b04080c9f2e81d44
Rana Muhammad Talha <muhammadtalhashahid2005@gmail.com>
2026-01-22 22:43:55 +0500

    HyperParameter Tuning and finding best model and params

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,8 @@
 import os
 import sys
 
-import numpy as np
-import pandas as pd
-
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 from src.logger import logging
 
@@ -30,14 +28,21 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    logging.info("Evaluating models...")
     try:
         report = {}
 
         for i in range(len(models)):
             model = list(models.values())[i]
+            param=params[list(models.keys())[i]]
 
+            # Perform Grid Search CV
+            gs = GridSearchCV(model, param, cv=5)
+            gs.fit(X_train, y_train)
+            
             # Train the model
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             # Predict on test data
